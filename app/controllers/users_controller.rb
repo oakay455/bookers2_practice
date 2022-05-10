fd237b6641067cfa2f13c59b096e3cfb47e8ef0e
oakay455 <ayako110.89@gmail.com>
2022-05-10 04:31:26 +0000

    [Add]アクセス制限追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])#必要なユーザー情報を取得
     @books = @user.books #特定のユーザ（@user）に関連付けられた投稿全て（.books）を取得し@booksに渡す
@@ -31,5 +34,12 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
